12c91deda7456a28bbfe4835e78c6ed252aa19ad
atlansien <atlansien@yahoo.co.jp>
2019-06-18 14:54:59 +0900

    Fix:つよぽんさんのコードを参考にしていたが、upとdownがなんとなくどういう動きか理解できたので自分なりのコードを作成

--- db/seeders/20190618043845-create-demo-todos.js
+++ db/seeders/20190618043845-create-demo-todos.js
@@ -2,13 +2,22 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    const demoTodos = [1,2,3,4,5].map(num => {
-      return {
-        title: `demo title ${num}`,
-        body: `I will show demo in meetup ${num}`,
+    // const demoTodos = [1,2,3,4,5].map(num => {
+    //   return {
+    //     title: `demo title ${num}`,
+    //     body: `I will show demo in meetup ${num}`,
+    //     completed: false
+    //   };
+    // });
+    let demoTodos = [];
+    for(let i = 0; i < 5; i++){
+      const todo = {
+        title: `demo title ${i}`,
+        body: `I will show demo in meetup ${i}`,
         completed: false
       };
-    });
+      demoTodos.push(todo);
+    }
 
     return queryInterface.bulkInsert("todos", demoTodos, {});
   },
