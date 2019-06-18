9803ea3b3a54835ab2e1d03463a393c7e9097a17
atlansien <atlansien@yahoo.co.jp>
2019-06-18 23:14:31 +0900

    Fix: テーブルに属性を追加、再migration

--- db/migrations/20190618043301-create-todos.js
+++ db/migrations/20190618043301-create-todos.js
@@ -6,16 +6,20 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER.UNSIGNED
       },
       title: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       body: {
+        allowNull: false,
         type: Sequelize.TEXT
       },
       completed: {
-        type: Sequelize.BOOLEAN
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
       },
       createdAt: {
         allowNull: false,
--- db/seeders/20190618043845-create-demo-todos.js
+++ db/seeders/20190618043845-create-demo-todos.js
@@ -2,13 +2,7 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    // const demoTodos = [1,2,3,4,5].map(num => {
-    //   return {
-    //     title: `demo title ${num}`,
-    //     body: `I will show demo in meetup ${num}`,
-    //     completed: false
-    //   };
-    // });
+
     let demoTodos = [];
     for(let i = 0; i < 5; i++){
       const todo = {
