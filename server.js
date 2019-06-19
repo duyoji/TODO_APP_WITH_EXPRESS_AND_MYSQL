df65315b9090ee0095634ba800e981fb3d7ba251
atlansien <atlansien@yahoo.co.jp>
2019-06-19 14:26:39 +0900

    Add:corsのインストール、morganの設定追加

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const logger = require("morgan");
+const cors = require("cors");
+
 const router = require("./resources/api.router");
 
 const app = express();
 
+app.use(logger("short"));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
