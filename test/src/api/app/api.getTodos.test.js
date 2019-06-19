bd861c828d3f9c6582a11db0865ec4892a8cfc0c
atlansien <atlansien@yahoo.co.jp>
2019-06-19 15:41:15 +0900

    Add:api.getTodos.test.js作成、テストコード記述。現在RED

---
+++ test/src/api/app/api.getTodos.test.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+const requestHelper = require("../../../../helper/requestHelper").request;
+const assert = require("power-assert");
+
+describe("test 「GET /api/todos」", () => {
+  it("getリクエストで返されたデータは適切である", async () => {
+    const response = await requestHelper({
+      method: "get",
+      endPoint: "/api/todos",
+      statusCode: 200,
+    });
+
+    // responseはjsonなので分解する
+    const todos = response.body;
+
+    // todosは配列である
+    assert.strictEqual(Array.isArray(todos), true);
+
+    todos.forEach(todo => {
+      assert.strictEqual(typeof todo.id, "number");
+      assert.strictEqual(typeof todo.title, "string");
+      assert.strictEqual(typeof todo.body, "string");
+      assert.strictEqual(typeof todo.createdAt, "string");
+      assert.strictEqual(typeof todo.updatedAt, "string");
+    });
+  });
+});
