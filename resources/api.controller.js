541654afe9f09fa8531c454f70dea6a2b28b9ccb
atlansien <atlansien@yahoo.co.jp>
2019-06-19 09:42:19 +0900

    Add: api.controller.jsにコード記述、サーバー解放後POSTMANにて接続の確認済み

--- resources/api.controller.js
+++ resources/api.controller.js
@@ -1,6 +1,14 @@
 module.exports = {
-  getTodos: () => {},
-  postTodo: () => {},
-  putTodo: () => {},
-  deleteTodo: () => {},
+  getTodos: (req, res) => {
+    res.send("It's GET request method");
+  },
+  postTodo: (req, res) => {
+    res.send("It's POST request method");
+  },
+  putTodo: (req, res) => {
+    res.send("It's PUT request method");
+  },
+  deleteTodo: (req, res) => {
+    res.send("It's DELETE request method");
+  },
 };
