7ed77aee14667f7e4d51a101086106cbf8739b17
atlansien <atlansien@yahoo.co.jp>
2019-06-19 09:25:40 +0900

    server.jsの作成

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = require("./resources/api.router");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/", router);
+
+module.exports = app;
