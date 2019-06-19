f4fd87cf049237188d73f6d2802a23a472456008
atlansien <atlansien@yahoo.co.jp>
2019-06-19 11:58:28 +0900

    Fix:api.controller.jsの変更。putTodoとdeleteTodoにてパラメーターを会得したことを確認するための記述を追加

--- resources/api.controller.js
+++ resources/api.controller.js
@@ -6,9 +6,9 @@ module.exports = {
     res.status(200).send("It's POST request method");
   },
   putTodo: (req, res) => {
-    res.status(200).send("It's PUT request method");
+    res.status(200).send(`It's PUT request method ID: ${req.params.id}`);
   },
   deleteTodo: (req, res) => {
-    res.status(200).send("It's DELETE request method");
+    res.status(200).send(`It's DELETE request method ID: ${req.params.id}`);
   },
 };
