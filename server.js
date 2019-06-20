6a1e65ce77f5d6a6ae578822c38f59b79d89d876
atlansien <atlansien@yahoo.co.jp>
2019-06-20 16:23:59 +0900

    Change:Body-parserをexpress.json()に変更/変更しても挙動に差がなかった、コード量の削減、データ量の削減

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const logger = require("morgan");
 const cors = require("cors");
 
@@ -9,8 +8,8 @@ const app = express();
 
 app.use(logger("short"));
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/todos", router);
 
