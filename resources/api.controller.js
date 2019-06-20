2af4a8ac751251029665ab0471b4b9c6b10ef816
atlansien <atlansien@yahoo.co.jp>
2019-06-20 21:25:17 +0900

    Add:completedにboolean型以外を入れた場合の挙動を追加（エラーになる）

--- resources/api.controller.js
+++ resources/api.controller.js
@@ -1,5 +1,15 @@
 const index = require("../db/models/index");
 
+const toBoolean = data => {
+  if (data.toLowerCase() === "true") {
+    return true;
+  } else if (data.toLowerCase() === "false") {
+    return false;
+  } else {
+    return undefined;
+  }
+};
+
 module.exports = {
   getTodos: async (req, res) => {
     try {
@@ -17,9 +27,14 @@ module.exports = {
         {
           title: req.body.title,
           body: req.body.body,
+          completed: req.body.completed,
         },
         { t }
       );
+      const completed = toBoolean(JSON.stringify(todo.completed));
+      if (typeof completed !== "boolean") {
+        throw new Error("completedにはboolean型を入力してください");
+      }
       await t.commit();
       res.status(200).json(todo);
     } catch (err) {
