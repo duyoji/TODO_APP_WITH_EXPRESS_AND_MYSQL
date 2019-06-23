b4bb3e1ce918c70b6fb8d04d730de4942e5069f8
atlansien <atlansien@yahoo.co.jp>
2019-06-23 17:07:33 +0900

    Add:api.putTodo.test.js/適切にデータを送った場合、idと合致したTodo一件のtitle、body、completed全て、またはいずれかが変更され返ってくる、また配列内にあったidと紐つくコメントは変更されたコメントに上書きされる

--- test/src/api/app/api.putTodo.test.js
+++ test/src/api/app/api.putTodo.test.js
@@ -80,6 +80,7 @@ describe("TEST 「PUT /api/todos/:id」", () => {
       `検索結果: ID${invalidId}に該当するTodoは見つかりませんでした`
     );
   });
+
   it("completedにboolean型以外を送った場合、エラーが返る", async () => {
     const invalidCompletedList = [-1, 2, "0", null, [], {}];
     const id = 3;
@@ -96,6 +97,98 @@ describe("TEST 「PUT /api/todos/:id」", () => {
         response.body.message,
         "completedにはboolean型のみを入力してください"
       );
+    }
+  });
+
+  it("適切にデータを送った場合、idと合致したTodo一件のtitle、body、completed全て、またはいずれかが変更され返ってくる、また配列内にあったidと紐つくコメントは変更されたコメントに上書きされる", async () => {
+    const validId = 3;
+
+    const oldTodos = await getTodos();
+
+    datas = [
+      { title: "title" },
+      { body: "body" },
+      { completed: true },
+      { title: "title", body: "body" },
+      { title: "title", completed: true },
+      { body: "body", completed: true },
+      { title: "title", body: "body", completed: true },
+    ];
+
+    for (let i = 0; i < 5; i++) {
+      const response = await updateTodo(200, validId, datas[i]);
+      const todo = response.body;
+
+      if (datas[i].title && datas[i].body && datas[i].completed) {
+        assert.deepStrictEqual(todo, {
+          id: validId,
+          title: datas[i].title,
+          body: datas[i].body,
+          completed: datas[i].completed,
+          createdAt: todo.createdAt,
+          updatedAt: todo.updatedAt,
+        });
+      } else if (datas[i].title && datas[i].body) {
+        assert.deepStrictEqual(todo, {
+          id: validId,
+          title: datas[i].title,
+          body: datas[i].body,
+          completed: todo.completed,
+          createdAt: todo.createdAt,
+          updatedAt: todo.updatedAt,
+        });
+      } else if (datas[i].title && datas[i].completed) {
+        assert.deepStrictEqual(todo, {
+          id: validId,
+          title: datas[i].title,
+          body: todo.body,
+          completed: datas[i].completed,
+          createdAt: todo.createdAt,
+          updatedAt: todo.updatedAt,
+        });
+      } else if (datas[i].body && datas[i].completed) {
+        assert.deepStrictEqual(todo, {
+          id: validId,
+          title: todo.title,
+          body: datas[i].body,
+          completed: datas[i].completed,
+          createdAt: todo.createdAt,
+          updatedAt: todo.updatedAt,
+        });
+      } else if (datas[i].title) {
+        assert.deepStrictEqual(todo, {
+          id: validId,
+          title: datas[i].title,
+          body: todo.body,
+          completed: todo.completed,
+          createdAt: todo.createdAt,
+          updatedAt: todo.updatedAt,
+        });
+      } else if (datas[i].body) {
+        assert.deepStrictEqual(todo, {
+          id: validId,
+          title: todo.title,
+          body: datas[i].body,
+          completed: todo.completed,
+          createdAt: todo.createdAt,
+          updatedAt: todo.updatedAt,
+        });
+      } else if (datas[i].completed) {
+        assert.deepStrictEqual(todo, {
+          id: validId,
+          title: todo.title,
+          body: datas[i].body,
+          completed: todo.completed,
+          createdAt: todo.createdAt,
+          updatedAt: todo.updatedAt,
+        });
+      }
+
+      assert.strictEqual(todo.updateTodo > todo.createdAt, true);
+
+      const currentTodos = await getTodos();
+
+      assert.notStrictEqual(oldTodos[2], currentTodos[2]);
     }
   });
 });
