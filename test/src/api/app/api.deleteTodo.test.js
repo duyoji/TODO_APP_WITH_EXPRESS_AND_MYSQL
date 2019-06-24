129077d14a8a84400a22a3f003651cd1f131b50e
atlansien <atlansien@yahoo.co.jp>
2019-06-24 16:07:13 +0900

    Add:api.deleteTodo.test.js

---
+++ test/src/api/app/api.deleteTodo.test.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+const assert = require("power-assert");
+
+const index = require("../../../../db/models/index");
+
+const DummyTodo = require("../../../../helper/createHelper");
+const requestHelper = require("../../../.././helper/requestHelper").request;
