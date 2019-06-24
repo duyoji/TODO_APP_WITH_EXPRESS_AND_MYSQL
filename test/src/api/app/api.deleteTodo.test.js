4b513bbbdb1c76e71b7e9400c690dc37e472ecaf
atlansien <atlansien@yahoo.co.jp>
2019-06-24 16:15:08 +0900

    Add:api.deleteTodo.test.js/テスト前段階記述

--- test/src/api/app/api.deleteTodo.test.js
+++ test/src/api/app/api.deleteTodo.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 const assert = require("power-assert");
 
@@ -5,3 +6,43 @@ const index = require("../../../../db/models/index");
 
 const DummyTodo = require("../../../../helper/createHelper");
 const requestHelper = require("../../../.././helper/requestHelper").request;
+
+// 記述終了後削除
+const chalk = require("chalk");
+
+const getTodos = async () => {
+  const response = await requestHelper({
+    method: "get",
+    endPoint: "/api/todos",
+    statusCode: 200,
+  });
+  return response;
+};
+
+const deleteTodo = async (code, id) => {
+  const response = await requestHelper({
+    method: "delete",
+    endPoint: `/api/todos/${id}`,
+    statusCode: 400,
+  });
+  return response;
+};
+
+describe("TEST 「DELETE /api/todos/:id」", () => {
+  before(async () => {
+    const promises = [];
+    for (let i = 0; i < 5; i++) {
+      const promise = await index.Todo.create(
+        new DummyTodo({
+          title: `title${i}`,
+          body: `body${i}`,
+        })
+      );
+      promises.push(promise);
+    }
+    await Promise.all(promises);
+  });
+  after(async () => {
+    index.Todo.truncate();
+  });
+});
