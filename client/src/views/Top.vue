9b49c3b05d0d9849c4996c2dceb286c6e08060ef
atlansien <atlansien@yahoo.co.jp>
2019-07-23 19:46:42 +0900

    add:Top.vueのcreatedにactions.fetchTodosをセット

--- client/src/views/Top.vue
+++ client/src/views/Top.vue
@@ -14,11 +14,18 @@
 import Header from "../components/Header.vue";
 import Input from "../components/Input.vue";
 import Todos from "../components/Todo/Todos.vue";
+import { mapActions } from "vuex";
 export default {
   components: {
     appHeader: Header,
     appInput: Input,
     appTodos: Todos
+  },
+  methods: {
+    ...mapActions["fetchTodos"]
+  },
+  created() {
+    this.fetchTodos();
   },
 };
 </script>
