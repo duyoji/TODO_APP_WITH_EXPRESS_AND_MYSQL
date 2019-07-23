7d28a129111efe512425dfa3bf791b83060ef45b
atlansien <atlansien@yahoo.co.jp>
2019-07-23 17:52:54 +0900

    lint

--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -3,13 +3,13 @@ import axios from "axios";
 const API_URL = "http://localhost:8040/api/todos";
 
 export default {
-	async fetchTodos({commit}) {
-		try {
-			const res = await axios.get(API_URL);
-			const todoData = res.data;
-			commit("setTodos", todoData);
-		}catch (error) {
-			throw new Error("APIエラーが発生しました")
-		}
-	}
-}
+  async fetchTodos({ commit }) {
+    try {
+      const res = await axios.get(API_URL);
+      const todoData = res.data;
+      commit("setTodos", todoData);
+    } catch (error) {
+      throw new Error("APIエラーが発生しました");
+    }
+  }
+};
