938463d9c6ec7ae1d8061a4a79c99ecfe359c227
atlansien <atlansien@yahoo.co.jp>
2019-07-24 11:36:39 +0900

    add:v-text-fieldにルール追加

--- client/src/components/Input.vue
+++ client/src/components/Input.vue
@@ -15,10 +15,10 @@
         </v-layout>
         <v-layout justify-center>
           <v-flex xs12 sm4 md5>
-            <v-text-field label="タイトルを入力してください" v-model="title"></v-text-field>
+            <v-text-field label="タイトルを入力してください"  :rules="inputRule" v-model="title"></v-text-field>
           </v-flex>
           <v-flex xs12 sm4 md5>
-            <v-text-field label="内容を入力してください" v-model="body"></v-text-field>
+            <v-text-field label="内容を入力してください" :rules="inputRule" v-model="body"></v-text-field>
           </v-flex>
           <v-flex xs12 sm4 md2>
             <v-btn outline align-center color="primary" dark @click="postTodoButton()">送信</v-btn>
@@ -39,6 +39,11 @@ export default {
       isError: false,
       errorMsg: ""
     };
+  },
+  computed: {
+    inputRule() {
+      return [v => !!v || "必ず入力してください"];
+    }
   },
   methods: {
     ...mapActions(["postTodo"]),
