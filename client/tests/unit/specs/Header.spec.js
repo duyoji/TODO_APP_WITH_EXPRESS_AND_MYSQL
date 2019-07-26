e8be126855f332afd3799afc533cb72ad73d10b9
atlansien <atlansien@yahoo.co.jp>
2019-07-26 11:23:55 +0900

    add:Header.spec.js

---
+++ client/tests/unit/specs/Header.spec.js
@@ -0,0 +1,9 @@
+import { mount } from "@vue/test-utils"
+import Header from "@/components/Header.vue"
+
+describe("TEST Header.vue", () => {
+	const wrapper = mount(Header);
+	it("ボタンの動作チェック", () => {
+		
+	})
+})
